dd57a20db952026024a77685800e4bc334f8dd65
Dave <davislyu@gmail.com>
2024-01-07 10:18:36 +0200

    Enhance with dynamic cell data& layout adjustments

--- SheetsProjVue/src/components/MainView.vue
+++ SheetsProjVue/src/components/MainView.vue
@@ -1,13 +1,16 @@
+/MainView.vue
 <template>
   <div>
     <WorkbookTabs
       :tabs="tabs"
-      @tabChanged="handleTabChange"
-      @addNewTab="addTab"
+      :activeTabId="activeTabId"
+      @tabChange="setActiveTab"
     />
     <Workbook
-      :rows="activeTabData.rows"
-      :cellContents="activeTabData.cellContents"
+      :cellData="activeTabData.cellContents"
+      :rows="rows"
+      :columns="columns"
+      @cellUpdate="handleCellUpdate"
     />
   </div>
 </template>
@@ -15,51 +18,31 @@
 <script>
 import WorkbookTabs from "./WorkbookTabs.vue";
 import Workbook from "./Workbook.vue";
-import { ref, reactive } from "vue";
 
 export default {
-  components: {
-    WorkbookTabs,
-    Workbook,
-  },
-  setup() {
-    const tabs = reactive([
-      {
-        id: 1,
-        rows: Array.from({ length: 100 }, (_, i) => i + 1),
-        cellContents: {},
-      },
-      {
-        id: 2,
-        rows: Array.from({ length: 100 }, (_, i) => i + 1),
-        cellContents: {},
-      },
-    ]);
-    const activeTabData = ref(tabs[0]);
-
-    function addTab(newTabId) {
-      const newTab = {
-        id: newTabId,
-        rows: Array.from({ length: 100 }, (_, i) => i + 1),
-        cellContents: {},
-      };
-      tabs.push(newTab);
-      activeTabData.value = newTab;
-    }
-
-    function handleTabChange(tabId) {
-      const newActiveTab = tabs.find((tab) => tab.id === tabId);
-      if (newActiveTab) {
-        activeTabData.value = newActiveTab;
-      }
-    }
-
+  components: { WorkbookTabs, Workbook },
+  data() {
     return {
-      tabs,
-      activeTabData,
-      handleTabChange,
-      addTab,
+      tabs: [{ id: 1, cellContents: {} }],
+      activeTabId: 1,
+      rows: Array.from({ length: 100 }, (_, i) => i + 1),
+      columns: 20,
     };
+  },
+  computed: {
+    activeTabData() {
+      return this.tabs.find((tab) => tab.id === this.activeTabId) || {};
+    },
+  },
+  methods: {
+    setActiveTab(tabId) {
+      this.activeTabId = tabId;
+    },
+
+    handleCellUpdate({ row, col, content }) {
+      this.activeTabData.cellContents[`${row}-${col}`] = content;
+      console.log(content);
+    },
   },
 };
 </script>
