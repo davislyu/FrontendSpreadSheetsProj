ef3d1675723eaf4f3732abe6e883969ba80c69e2
Dave <davislyu@gmail.com>
2024-01-07 10:13:52 +0200

    Refactored for improved  functionality and style

--- SheetsProjVue/src/components/WorkbookTabs.vue
+++ SheetsProjVue/src/components/WorkbookTabs.vue
@@ -1,92 +1,42 @@
-//workbookTabs.vue
 <template>
-  <div class="main-container">
-    <div>
-      <button @click="toggleActiveTabStatus">
-        {{ tabsActive ? "Hide tabs" : "Open tabs" }}
-      </button>
-    </div>
-    <div v-if="tabsActive" class="tabs-container">
-      <ul class="tabs-list">
-        <li
-          v-for="tab in tabs"
-          :key="tab.id"
-          :class="{ active: tab.id === activeTabId }"
-          @click="() => setActiveTab(tab.id)"
-        >
-          Tab {{ tab.id }}
-        </li>
-        <button @click="addTab">+</button>
-      </ul>
-    </div>
+  <div class="tabs-container">
+    <ul class="tabs-list">
+      <li
+        v-for="tab in tabs"
+        :key="tab.id"
+        :class="{ active: tab.id === activeTabId }"
+        @click="setActiveTab(tab.id)"
+      >
+        Tab {{ tab.id }}
+      </li>
+      <button @click="addTab">+</button>
+    </ul>
   </div>
 </template>
 
 <script>
-import { ref, watch, defineComponent } from "vue";
-
-export default defineComponent({
-  props: {
-    tabs: Array,
+export default {
+  props: ["tabs", "activeTabId"],
+  emits: ["tabChange"],
+  methods: {
+    setActiveTab(tabId) {
+      this.$emit("tabChange", tabId);
+    },
+    addTab() {
+      const newTabId = this.tabs.length + 1;
+      this.tabs.push({ id: newTabId, cellContents: {} });
+      this.setActiveTab(newTabId);
+    },
   },
-  setup(props, { emit }) {
-    const activeTabId = ref(props.tabs[0].id);
-    const tabsActive = ref(true);
-
-    const addTab = () => {
-      const newTabId = props.tabs[props.tabs.length - 1].id + 1;
-      emit("addNewTab", newTabId);
-      setActiveTab(newTabId);
-    };
-
-    const toggleActiveTabStatus = () => {
-      tabsActive.value = !tabsActive.value;
-    };
-
-    const setActiveTab = (tabId) => {
-      activeTabId.value = tabId;
-      emit("tabChanged", tabId);
-    };
-
-    watch(
-      () => props.tabs,
-      (newTabs) => {
-        if (!newTabs.find((tab) => tab.id === activeTabId.value)) {
-          activeTabId.value = newTabs[0].id;
-        }
-      }
-    );
-
-    return {
-      activeTabId,
-      setActiveTab,
-      addTab,
-      tabsActive,
-      toggleActiveTabStatus,
-    };
-  },
-});
+};
 </script>
-
 <style scoped lang="sass">
+.active
+  text-decoration: underline
 .tabs-list
-  display: flex
-  gap: 1rem
-  li
     list-style: none
-    &:hover
-      cursor: pointer
-  button
-    border: none
-    background-color: transparent
-    font-size: 1rem
-    &:hover
-        cursor: pointer
-  .active
-    text-decoration: underline
-.main-container
     display: flex
-    align-items: center
-    padding: 0
-    margin: 0
+    gap: 1rem
+    li:hover,button:hover
+        cursor: pointer
 </style>
