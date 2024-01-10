80e8e2ae455e9a2dee7d0329d591b9788747cbab
Dave <davislyu@gmail.com>
2024-01-10 11:18:09 +0200

    Button imports

--- SheetsProjVue/src/main.js
+++ SheetsProjVue/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faFileCirclePlus } from '@fortawesome/free-solid-svg-icons'
-library.add(faFileCirclePlus)
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPlus, faBars)
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
