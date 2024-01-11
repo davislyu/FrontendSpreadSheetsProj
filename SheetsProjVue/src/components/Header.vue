93df094dc1b7211fb354f653db4819633a7ccd2e
Dave <davislyu@gmail.com>
2024-01-11 13:08:08 +0200

    Header comp add

---
+++ SheetsProjVue/src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header-container">
+    <header class="header-title-section">
+      <i class="header-title-sheetsLogo">
+        <img src="../assets/sheetsLogo.png" alt="#" />
+      </i>
+      <input
+        type="text"
+        placeholder="United Tabular Sheet"
+        class="header-title-text"
+        @input="adjustWidth"
+        :size="inputSize"
+        ref="input"
+      />
+    </header>
+    <div class="header-pfPic">
+      <img src="../assets/boris.png" alt="#" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputSize: 1, // Initial size
+    };
+  },
+  methods: {
+    adjustWidth() {
+      let input = this.$refs.input;
+      this.inputSize =
+        Math.max(input.value.length, input.placeholder.length) || 10;
+    },
+  },
+  mounted() {
+    this.adjustWidth();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/Header.scss";
+</style>
