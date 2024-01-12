0f23ec893f29f38e0ed1d06a10c2ffa3dd0dd264
Dave <davislyu@gmail.com>
2024-01-12 22:16:51 +0200

    Added x icon

--- SheetsProjVue/src/main.js
+++ SheetsProjVue/src/main.js
@@ -4,8 +4,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faPlus, faBars)
+
+library.add(faPlus, faBars, faXmark)
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
