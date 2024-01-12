a623cf73ac1e1da052bf0d6396a61fac40b9f2ab
Dave <davislyu@gmail.com>
2024-01-12 22:17:34 +0200

    Toggle tabs feature edit

--- SheetsProjVue/src/components/WorkbookTabs.vue
+++ SheetsProjVue/src/components/WorkbookTabs.vue
@@ -1,50 +1,84 @@
+//Workbooktabs.vue
 <template>
-  <div class="tab-navigation-container">
-    <font-awesome-icon
-      @click="toggleTabs"
-      class="tab-icon burger-icon"
-      icon="fa-solid fa-bars"
-    />
-    <Transition name="slide-fade">
-      <ul v-if="tabsActive" class="tab-navigation-list">
-        <li
-          v-for="tab in tabs"
-          :key="tab.id"
-          :class="{ active: tab.id === activeTabId }"
-          @click="setActiveTab(tab.id)"
-        >
-          Tab {{ tab.id }}
-        </li>
-      </ul>
-    </Transition>
-    <button @click="addTab" class="add-tab-button">
-      <font-awesome-icon class="tab-icon plus-icon" icon="fa-solid fa-plus" />
-    </button>
+  <div class="tab-wrapper">
+    <div class="tab-navigation-container">
+      <font-awesome-icon
+        @click="toggleTabs"
+        class="tab-icon burger-icon"
+        icon="fa-solid fa-bars"
+      />
+      <Transition name="slide-fade">
+        <ul v-if="tabsActive" class="tab-navigation-list">
+          <li
+            v-for="tab in tabs"
+            :key="tab.id"
+            :class="{ active: tab.id === activeTabId }"
+            @click="setActiveTab(tab.id)"
+          >
+            Tab {{ tab.id }}
+            <span @click.stop="tabToDelete(tab.id)">
+              <font-awesome-icon
+                class="tab-close-btn"
+                icon="fa-solid fa-xmark"
+              />
+            </span>
+          </li>
+        </ul>
+      </Transition>
+      <button @click="addTab" class="add-tab-button">
+        <font-awesome-icon class="tab-icon plus-icon" icon="fa-solid fa-plus" />
+      </button>
+    </div>
+    <div class="tab-settings">
+      <form action="submit">
+        <input
+          id="cell-finder-input"
+          type="text"
+          :placeholder="someDataProperty"
+        />
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import { eventBus } from "../eventBus";
+
 export default {
   props: ["tabs", "activeTabId"],
-  emits: ["tabChange"],
+  emits: ["tabChange", "tabToDelete"],
   data() {
     return {
       tabsActive: true,
+      someDataProperty: "",
     };
   },
+
   methods: {
     setActiveTab(tabId) {
       this.$emit("tabChange", tabId);
+      eventBus.emit("tabChanged");
+      this.someDataProperty = null;
     },
     addTab() {
-      const newTabId = this.tabs.length + 1;
-      this.tabs.push({ id: newTabId, cellContents: {} });
-      this.setActiveTab(newTabId);
+      this.$emit("addTab");
       this.tabsActive = true;
     },
+
     toggleTabs() {
       this.tabsActive = !this.tabsActive;
     },
+    tabToDelete(tabId) {
+      this.$emit("tabToDelete", tabId);
+    },
+  },
+  mounted() {
+    eventBus.on("focusedCellChange", (data) => {
+      this.someDataProperty = data;
+    });
+  },
+  unmounted() {
+    eventBus.off("focusedCellChange");
   },
 };
 </script>
